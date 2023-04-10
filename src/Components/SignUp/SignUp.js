bf23c1b0c70f75cc2ce60b68a96b29f70bfb1626
Elyor <ilhomovelyor20010624@gmail.com>
2023-04-10 14:44:12 +0500

    some change

--- src/Components/SignUp/SignUp.js
+++ src/Components/SignUp/SignUp.js
@@ -1,10 +1,30 @@
 import React from 'react'
+import { TbBrandFacebook } from 'react-icons/tb'
 import './SignUp.css'
 
 const SignUp = () => {
 return (
-    <div>
-        
+    <div className='container'>
+        <div className="signUp-card">
+            <div className="signUp-logo flex">
+                <h1>SIGN UP</h1>
+                <p>Welcome To Zaprafka.uz</p>
+            </div>
+            <div className="signUp-inputs flex">
+                <input type="text" />
+                <input type="text" />
+                <input type="text" />
+                <input type="text" />
+                <button>SIGN UP</button>
+            </div>
+            <div className="signUp-footer flex">
+                <div className="facebook">
+                    <TbBrandFacebook  className='icon'/>
+                </div>
+                <div className="google"></div>
+                <div className="twiter"></div>
+            </div>
+        </div>
     </div>
 )
 }
